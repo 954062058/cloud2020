b167b128e543bcd3489a1ae42911ceffe239cd3c
nishaozhou <954062058@qq.com>
2021-02-03 15:34:32 +0800

    测试

--- cloud-consumerzk-order80/src/main/java/com/ns/springcloud/Comtroller/OrderZKController.java
+++ cloud-consumerzk-order80/src/main/java/com/ns/springcloud/Comtroller/OrderZKController.java
@@ -3,16 +3,9 @@ package com.ns.springcloud.Comtroller;
 import com.ns.springcloud.entities.CommonResult;
 import com.ns.springcloud.entities.Payment;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cloud.client.ServiceInstance;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
-
 import javax.annotation.Resource;
-import java.util.List;
-import java.util.UUID;
 
 /**
  * @author : Nsz
