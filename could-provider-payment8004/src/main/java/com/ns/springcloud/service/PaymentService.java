330cf94e302c001934b1a7bd72762602602783af
nishaozhou <954062058@qq.com>
2021-02-03 10:23:36 +0800

    测试

---
+++ could-provider-payment8004/src/main/java/com/ns/springcloud/service/PaymentService.java
@@ -0,0 +1,17 @@
+package com.ns.springcloud.service;
+
+import com.ns.springcloud.entities.Payment;
+import org.apache.ibatis.annotations.Param;
+
+/**
+ * @author : Nsz
+ * @version :1.0
+ * @email : 954062058@qq.com
+ * @since : 2021-01-27 10:31
+ */
+
+public interface PaymentService {
+    Payment create(Payment payment);
+
+    Payment getPaymentById(@Param("id") Long id);
+}
